d00f4c684ff83ddddca2a76265bb9b5ac17fdacf
choimung <103022503+choimung@users.noreply.github.com>
2023-06-08 23:02:59 +0900

    기본적인html

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ import html
 
 app = Flask(__name__)
 
+# TODO 홈페이지 주소로 연결할 수 있게 하기
+
+
 # 페이지별로 축제정보 렌더링
 @app.route('/festivals')
 def page_view():
     end_page = 2
-    return render_template('festivals.html', festivals=get_festivals(end_page))
+    return render_template('festival.html', festivals=get_festivals(end_page))
 
-def get_festivals(page_number, search_query=None, year='2023'):
+
+def get_festivals(end_page, search_query=None, location_search=None, year=None, month=None):
     url = 'http://api.data.go.kr/openapi/tn_pubr_public_cltur_fstvl_api'
     festivals = []
 
-    for page in range(1, page_number + 1):
+    for page in range(1, end_page + 1):
         params = {
             'serviceKey': 'PH9LodLRmVyqEv1l/OVszYUfRG4OtB21PyMWnlDD/1cf/PSLnO4GONguUMsbMlXDmSMzwHWK4/6YSifuhD5kKA==',
             'pageNo': page,
@@ -30,11 +34,18 @@ def get_festivals(page_number, search_query=None, year='2023'):
 
             for item in root.findall('.//item'):
                 festival_start_year = html.unescape(item.find('fstvlStartDate').text).split("-")
-                if festival_start_year[0] != year:
+                if year and festival_start_year[0] != year:
+                    continue
+
+                if month and festival_start_year[1] != month:
                     continue
 
                 festival_name = html.unescape(item.find('fstvlNm').text)
                 if search_query and search_query.lower() not in festival_name.lower():
+                    continue
+
+                festival_location = html.unescape(item.find('rdnmadr').text or item.find('lnmadr').text)
+                if location_search and location_search not in festival_location:
                     continue
 
                 festival = {
@@ -48,7 +59,6 @@ def get_festivals(page_number, search_query=None, year='2023'):
                 festivals.append(festival)
 
     return festivals
-
 
 
 # def get_festivals(page_number, search_query=None, year = '2023'):
