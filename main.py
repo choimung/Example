7d0d8e09ae7669631adf09ab38708fa451719c4d
choimung <103022503+choimung@users.noreply.github.com>
2023-06-11 15:47:47 +0900

    아님

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import html
 app = Flask(__name__)
 
 
+<<<<<<< HEAD
 @app.route('/')
 def index():
     return f"hello"
@@ -13,22 +14,45 @@ def index():
 @app.route('/festivals')
 def search_festivals():
     end_page = 1;
+=======
+# TODO 홈페이지 주소로 연결할 수 있게 하기
+# TODO 검색결과 false시 처리
+
+@app.route('/festivals')
+def search_festivals():
+>>>>>>> edcb8010bf41af5490414fa5fb44c9e358aaea91
     year = request.args.get('year')
     month = request.args.get('month')
     search = request.args.get('search')
 
+<<<<<<< HEAD
     festivals, search_results = get_festivals(end_page= end_page,search_query=search, year=year, month=month)
     return render_template('festival.html', festivals=festivals, search_results=search_results)
 
+=======
+    festivals, search_results = get_festivals(search_query=search, year=year, month=month)
+    return render_template('festival.html', festivals=festivals, search_results=search_results)
+
+  #  return render_template('festival.html', festivals=get_festivals(search_query=search,year=year,month=month))
+>>>>>>> edcb8010bf41af5490414fa5fb44c9e358aaea91
 
 
 # 페이지별로 축제정보 렌더링
 @app.route('/festivals')
 def page_view():
+<<<<<<< HEAD
     return render_template('festival.html', festivals=get_festivals(end_page))
 
 
 def get_festivals(end_page=1, search_query=None, year=None, month=None):
+=======
+    end_page = 1
+    return render_template('festival.html', festivals=get_festivals(end_page))
+
+
+def get_festivals(end_page=1, search_query=None, location_search=None, year=None, month=None):
+
+>>>>>>> edcb8010bf41af5490414fa5fb44c9e358aaea91
     search_results = False
 
     url = 'http://api.data.go.kr/openapi/tn_pubr_public_cltur_fstvl_api'
@@ -50,7 +74,11 @@ def get_festivals(end_page=1, search_query=None, year=None, month=None):
             for item in root.findall('.//item'):
                 festival_start_year = html.unescape(item.find('fstvlStartDate').text).split("-")
                 if year and festival_start_year[0] != year:
+<<<<<<< HEAD
                     # search_results = False
+=======
+                    search_results = False
+>>>>>>> edcb8010bf41af5490414fa5fb44c9e358aaea91
                     continue
 
                 if month and festival_start_year[1] != month:
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -10,3 +11,110 @@ def index():
 
 if __name__ == '__main__':
     app.run()
+=======
+# from flask import Flask, jsonify, render_template, request
+# import requests
+# import xml.etree.ElementTree as ET
+# import html
+#
+# app = Flask(__name__)
+#
+#
+# # 페이지 렌더링 부분
+# @app.route("/")
+# def get_main_form():
+#     return render_template("main.html")
+#
+#
+# @app.route('/festivals/<pageNo>')
+# def find_festivals_page(pageNo):
+#     return get_festivals(pageNo)
+#
+#
+# def init():
+#
+#
+# # api 호출후 요청 결과 가져오기
+# # pageno -> 페이지번호
+# # fstvlnm -> 축제명
+# # year -> 연
+# # month -> 월
+# # day -> 일
+# # insttcode > 기관코드
+#
+#
+# def get_festivals(pageNo="", fstvlName="", year="", month="", day="", insttCode=""):
+#     url = 'http://api.data.go.kr/openapi/tn_pubr_public_cltur_fstvl_api'
+#     params = {
+#         'serviceKey': 'PH9LodLRmVyqEv1l/OVszYUfRG4OtB21PyMWnlDD/1cf/PSLnO4GONguUMsbMlXDmSMzwHWK4/6YSifuhD5kKA==',
+#         'pageNo': pageNo,
+#         'type': 'xml',
+#     }
+#
+#     fstvlName = request.args.get("fstvlName")
+#
+#     response = requests.get(url, params=params)
+#     response.encoding = 'utf-8'
+#
+#     festivalName = []  # 축제이름을 저장하는 배열
+#     festivalVenue = []  # 개최장소를 저장하는 배열
+#     fstvlStartDate = []  # 축제시작일를 저장하는 배열
+#     fstvlEndDate = []  # 축제종료일를 저장하는 배열
+#     fstvlCo = []  # 축제소개를 저장하는 배열
+#     location = []  # 축제장소를 저장하는 배열
+#
+#     # 응답결과가 성공(200) 일때 실행
+#     if response.status_code == 200:
+#         xml_data = response.text
+#         root = ET.fromstring(xml_data)
+#
+#         for item in root.findall('.//item'):
+#             # html 태그의 값을 찾아오고 변수에 값을 저장해준다.
+#             fstvlNm = item.find('fstvlNm').text  # fsvmlNm 태그의 값을 찾고 값 을  가져온다.
+#             opar = item.find('opar').text  # opar 태그의 값을 찾고 값 을  가져온다.
+#             StartDate = item.find('fstvlStartDate').text  # StartDate 태그의 값을 찾고 값 을  가져온다.
+#             EndDate = item.find('fstvlEndDate').text  # EndDate 태그의 값을 찾고 값 을  가져온다.
+#             fstvlComent = item.find('fstvlCo').text  # fstvlComent 태그의 값을 찾고 값 을  가져온다.
+#             auspcInstt = item.find('auspcInstt').text  # auspcInstt 태그의 값을 찾고 값 을  가져온다.
+#             rdnmadr = item.find('rdnmadr').text  # auspcInstt 태그의 값을 찾고 값 을  가져온다.
+#
+#             if fstvlName in fstvlNm:
+#                 print("gg")
+#                 if (rdnmadr != None):
+#                     decoded_name = html.unescape(rdnmadr)
+#                     location.append(decoded_name)
+#                 else:
+#                     lnmadr = item.find('lnmadr').text  # auspcInstt 태그의 값을 찾고 값 을  가져온다.
+#                     decoded_name = html.unescape(lnmadr)
+#                     location.append(decoded_name)
+#
+#                 decode_and_append(fstvlNm, festivalName)
+#                 decode_and_append(opar, festivalVenue)
+#                 decode_and_append(StartDate, fstvlStartDate)
+#                 decode_and_append(EndDate, fstvlEndDate)
+#                 decode_and_append(fstvlComent, fstvlCo)
+#                 decode_and_append(auspcInstt, location)
+#             else:
+#                 get_festivals(pageNo=str(int(pageNo) + 1), fstvlName=fstvlName, year=year, month=month, day=day,
+#                                      insttCode=insttCode)
+#
+#     return render_template('result.html',
+#                            festivalName=festivalName, festivalVenue=festivalVenue, fstvlStartDate=fstvlStartDate,
+#                            fstvlEndDate=fstvlEndDate, fstvlCo=fstvlCo, location=location, address=location)
+#
+#
+# def decode_and_append(value, array):
+#     decoded_value = html.unescape(value)
+#     array.append(decoded_value)
+#
+#
+# if __name__ == '__main__':
+#
+#     app.run()
+#
+# # my_list = ["apple", "banana", "orange", "grape"]
+# #
+# # for i, item in enumerate(my_list):
+# #     if "ba" in item:
+# #         print("문자열 'ba'가 배열의", i, "번째 인덱스에 위치합니다.")
+>>>>>>> edcb8010bf41af5490414fa5fb44c9e358aaea91
